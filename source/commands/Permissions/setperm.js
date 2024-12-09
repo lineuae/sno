559cbd2e1b0170e2c92840ef405ab8728f0dacd7
lineuae <line.uae@proton.me>
2024-12-09 23:40:45 +0000

    Update setperm.js

--- source/commands/Permissions/setperm.js
+++ source/commands/Permissions/setperm.js
@@ -1,31 +1,30 @@
 module.exports = {
     name: 'setperm',
-    description: 'Attribue un rôle à une permission.',
+    description: 'Attribue un ou plusieurs rôles à une permission.',
     usage: {
-        fr: {'setperm <permission> <role>': "Attribue un rôle à une permission."},
+        fr: {'setperm <permission> <roles>': "Attribue un ou plusieurs rôles à une permission."},
         en: {
-            'setperm <permission> <role>': "Assigns a role to a permission."
+            'setperm <permission> <roles>': "Assigns one or more roles to a permission."
         }
     },
     run: async (client, message, args) => {
 
-
-        if (args.length !== 2) {
-            return message.channel.send(`\`❌\` Erreur: \`${client.prefix}setperm (1/2/3/4/5/6/7/8/9) @role\``);
+        if (args.length < 2) {
+            return message.channel.send(`\`❌\` Erreur: \`${client.prefix}setperm (1/2/3/4/5/6/7/8/9) @role1 @role2 ...\``);
         }
 
         const permission = args[0];
-        const role = message.mentions.roles.first() || message.guild.roles.cache.get(args[1]);
+        const roles = message.mentions.roles.map(role => role) || args.slice(1).map(roleId => message.guild.roles.cache.get(roleId));
 
-        if (!permission || !role) {
-            return message.channel.send('\`❌\` Erreur: Permission ou rôle invalide.');
+        if (!permission || !roles.length) {
+            return message.channel.send('\`❌\` Erreur: Permission ou rôles invalides.');
         }
 
         const permissionIndex = await client.db.get(`perms_${message.guild.id}`) || {};
         if (permissionIndex["perm" + permission]) {
-            permissionIndex["perm" + permission].role = role.id;
+            permissionIndex["perm" + permission].roles = roles.map(role => role.id);
             await client.db.set(`perms_${message.guild.id}`, permissionIndex);
-            message.channel.send(`Le rôle associé à la permission \`${permission}\` a été mis à jour vers \`${role.name}\`.`);
+            message.channel.send(`Les rôles associés à la permission \`${permission}\` ont été mis à jour vers \`${roles.map(role => role.name).join(', ')}\`.`);
         } else {
             message.channel.send(`\`❌\` Erreur: La permission \`${permission}\` n'existe pas.`);
         }
