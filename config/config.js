25a9e0812de45da3fb60d51dc2ef2bdb2877a308
lineuae <line.uae@proton.me>
2024-12-09 06:18:50 +0000

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "TOKEN",
+  token: "MTMxNTQ5NTI5MjY2OTI2Mzg5NA.G9XdF0.SKem6TSQieAPcd533Cq8pQAkLmHFMD0MVP3tF4",
   botId: "1315495292669263894",
   buyers: ["403174893707067392"],
   prefix: "-",
